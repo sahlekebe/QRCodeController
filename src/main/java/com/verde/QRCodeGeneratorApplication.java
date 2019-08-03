a33def96e9955cc0c97432aa46159bf1201aca2a
akalewoldk <>
2019-08-03 21:56:07 +0200

    Initial commit

---
+++ src/main/java/com/verde/QRCodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.verde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QRCodeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QRCodeGeneratorApplication.class, args);
+	}
+
+}
